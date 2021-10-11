de0e3c3c6a56855660395947b9c4da61f7fbc056
Sameera <samhas9000@gmail.com>
2021-10-12 01:55:27 +0800

    Update trainer_ae.py

--- trainer/trainer_ae.py
+++ trainer/trainer_ae.py
@@ -106,9 +106,9 @@ class Trainer:
                 checkpoint = torch.load(path)
                 self.mem_n2n.load_state_dict(checkpoint['model_state_dict'])
                 self.opt.load_state_dict(checkpoint['optimizer_state_dict'])
-                self.start_epoch = checkpoint['epoch']
+                self.start_epoch = checkpoint['epoch'] + 1
                 self.criterionLoss = checkpoint['loss']
-            break
+            
             
 
         # Write details to file
