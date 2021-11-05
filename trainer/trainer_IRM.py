2683fe2e82ccd9ed142d0473dd425916d59313d9
Sameera <samhas9000@gmail.com>
2021-11-06 02:59:10 +0800

    Update trainer_IRM.py

--- trainer/trainer_IRM.py
+++ trainer/trainer_IRM.py
@@ -75,8 +75,7 @@ class Trainer:
             "dim_embedding_key": config.dim_embedding_key,
             "num_prediction": self.num_prediction,
             "past_len": config.past_len,
-            "future_len": config.future_len,
-            "att_size": config.att_size
+            "future_len": config.future_len
         }
         self.max_epochs = config.max_epochs
 
@@ -111,7 +110,9 @@ class Trainer:
         self.writer.add_text('Training Configuration', 'learning rate init: ' + str(self.config.learning_rate), 0)
         self.writer.add_text('Training Configuration', 'dim_embedding_key: ' + str(self.settings["dim_embedding_key"]),
                              0)
-
+        
+        # self.writer.add_graph(self.model)
+    
     def write_details(self):
         """
         Serialize configuration parameters to file.
@@ -142,14 +143,7 @@ class Trainer:
             param.requires_grad = False
         for param in self.mem_n2n.decoder.parameters():
             param.requires_grad = False
-            
-            
-        # for param in self.mem_n2n.att1.parameters():
-        #     param.requires_grad = False
-        
-        # for param in self.mem_n2n.att2.parameters():
-        #     param.requires_grad = False
-            
+
         for param in self.mem_n2n.FC_output.parameters():
             param.requires_grad = False
         for param in self.mem_n2n.convScene_1.parameters():
